8b18a9f147b63e95e6cebb7aca4d80849427f716
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2019-01-21 13:56:46 -0700

    Reimport ARCore to fix issues

--- ARCore_Test/Assets/Scripts/ThesisController.cs
+++ ARCore_Test/Assets/Scripts/ThesisController.cs
@@ -18,19 +18,17 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-namespace GoogleARCore.Examples.AugmentedImage
-{
+namespace GoogleARCore.Examples.AugmentedImage {
     using System.Collections.Generic;
     using System.Runtime.InteropServices;
     using GoogleARCore;
-    using UnityEngine;
     using UnityEngine.UI;
+    using UnityEngine;
 
     /// <summary>
     /// Controller for AugmentedImage example.
     /// </summary>
-    public class ThesisController : MonoBehaviour
-    {
+    public class ThesisController : MonoBehaviour {
         /// <summary>
         /// List of prefabs to associate with images
         /// </summary>
@@ -41,65 +39,55 @@ namespace GoogleARCore.Examples.AugmentedImage
         /// </summary>
         public GameObject FitToScanOverlay;
 
-        private Dictionary<int, ThesisVisualizer> m_Visualizers
-            = new Dictionary<int, ThesisVisualizer>();
+        private Dictionary<int, ThesisVisualizer> m_Visualizers = new Dictionary<int, ThesisVisualizer> ();
 
-        private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+        private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage> ();
 
         /// <summary>
         /// The Unity Update method.
         /// </summary>
-        public void Update()
-        {
+        public void Update () {
             // Exit the app when the 'back' button is pressed.
-            if (Input.GetKey(KeyCode.Escape))
-            {
-                Application.Quit();
+            if (Input.GetKey (KeyCode.Escape)) {
+                Application.Quit ();
             }
 
             // Check that motion tracking is tracking.
-            if (Session.Status != SessionStatus.Tracking)
-            {
+            if (Session.Status != SessionStatus.Tracking) {
                 return;
             }
 
             // Get updated augmented images for this frame.
-            Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+            Session.GetTrackables<AugmentedImage> (m_TempAugmentedImages, TrackableQueryFilter.Updated);
 
             // Create visualizers and anchors for updated augmented images that are tracking and do not previously
             // have a visualizer. Remove visualizers for stopped images.
-            foreach (var image in m_TempAugmentedImages)
-            {
+            foreach (var image in m_TempAugmentedImages) {
                 ThesisVisualizer visualizer = null;
-                m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
-                if (image.TrackingState == TrackingState.Tracking && visualizer == null)
-                {
+                m_Visualizers.TryGetValue (image.DatabaseIndex, out visualizer);
+                if (image.TrackingState == TrackingState.Tracking && visualizer == null) {
                     // Create an anchor to ensure that ARCore keeps tracking this augmented image.
-                    Anchor anchor = image.CreateAnchor(image.CenterPose);
-                    visualizer = (ThesisVisualizer)Instantiate(Prefabs[image.DatabaseIndex], anchor.transform);
-                    Debug.Log("Created Visualizer: " + visualizer);
-                    Debug.Log(visualizer.transform.position.x + " " + visualizer.transform.position.x + " " + visualizer.transform.position.z);
+                    Anchor anchor = image.CreateAnchor (image.CenterPose);
+                    visualizer = (ThesisVisualizer) Instantiate (Prefabs[image.DatabaseIndex], anchor.transform);
+                    Debug.Log ("Created Visualizer: " + visualizer);
+                    Debug.Log (visualizer.transform.position.x + " " + visualizer.transform.position.x + " " + visualizer.transform.position.z);
                     visualizer.Image = image;
-                    m_Visualizers.Add(image.DatabaseIndex, visualizer);
-                }
-                else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
-                {
-                    m_Visualizers.Remove(image.DatabaseIndex);
-                    GameObject.Destroy(visualizer.gameObject);
+                    m_Visualizers.Add (image.DatabaseIndex, visualizer);
+                } else if (image.TrackingState == TrackingState.Stopped && visualizer != null) {
+                    m_Visualizers.Remove (image.DatabaseIndex);
+                    GameObject.Destroy (visualizer.gameObject);
                 }
             }
 
             // Show the fit-to-scan overlay if there are no images that are Tracking.
-            foreach (var visualizer in m_Visualizers.Values)
-            {
-                if (visualizer.Image.TrackingState == TrackingState.Tracking)
-                {
-                    FitToScanOverlay.SetActive(false);
+            foreach (var visualizer in m_Visualizers.Values) {
+                if (visualizer.Image.TrackingState == TrackingState.Tracking) {
+                    FitToScanOverlay.SetActive (false);
                     return;
                 }
             }
 
-            FitToScanOverlay.SetActive(true);
+            FitToScanOverlay.SetActive (true);
         }
     }
 }
