1c3d5ee0371c0cbf73f4cc9ddd55d4c65f0d854c
Nicholas Sundermeyer <nicsunde@cs.du.edu>
2019-03-26 15:42:50 -0700

    Starting GUI raycast

--- ARCore_Test/Assets/Scripts/InputSystem/ARInputManager.cs
+++ ARCore_Test/Assets/Scripts/InputSystem/ARInputManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using GoogleARCore;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 #if UNITY_EDITOR
 // Set up touch input propagation while using Instant Preview in the editor.
@@ -29,6 +30,12 @@ public class ARInputManager : MonoBehaviour {
 
 	// currently controlled object
 	private ARInteractable currentObject;
+
+	private GraphicRaycaster m_GraphicRaycaster;
+
+	void Start() {
+		m_GraphicRaycaster = GetComponent<GraphicRaycaster>();
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -58,27 +65,32 @@ public class ARInputManager : MonoBehaviour {
 	}
 
 	public void TouchDown (Vector2 pos) {
+		GameObject obj;
+
+		obj = RaycastGUI(pos);
+		if(obj != null) {
+			Debug.Log("UI object found: " + obj.name);
+			currentObject = obj.GetComponentInParent<ARInteractable>();
+		}
 
 		// raycast to check object being touched
-		if(Raycast (pos))
-		{
-			DebugWithoutRepeats("TouchDown" + pos);
+		if (Raycast (pos)) {
+			// DebugWithoutRepeats ("TouchDown" + pos);
 			// create event data and send to object
-			PointerEventData pData = new PointerEventData(EventSystem.current);
+			PointerEventData pData = new PointerEventData (EventSystem.current);
 			pData.position = pos;
-			currentObject.TouchDown(pData);
+			currentObject.TouchDown (pData);
 		}
 	}
 
 	public void TouchUp (Vector2 pos) {
 
 		// if there is currently a controlled object, nullify it and call touchUp
-		if(currentObject != null)
-		{
-			DebugWithoutRepeats ("TouchUp" + pos);
-			PointerEventData pData = new PointerEventData(EventSystem.current);
+		if (currentObject != null) {
+			// DebugWithoutRepeats ("TouchUp" + pos);
+			PointerEventData pData = new PointerEventData (EventSystem.current);
 			pData.position = pos;
-			currentObject.TouchUp(pData);
+			currentObject.TouchUp (pData);
 
 			currentObject = null;
 		}
@@ -86,14 +98,32 @@ public class ARInputManager : MonoBehaviour {
 
 	public void TouchClick (Vector2 pos) {
 
-		if(Raycast (pos))
-		{
-			DebugWithoutRepeats ("TouchClick" + pos);
+		if (Raycast (pos)) {
+			// DebugWithoutRepeats ("TouchClick" + pos);
 			// create event data and send to object
-			PointerEventData pData = new PointerEventData(EventSystem.current);
+			PointerEventData pData = new PointerEventData (EventSystem.current);
 			pData.position = pos;
-			currentObject.TouchClick(pData);
+			currentObject.TouchClick (pData);
 		}
+	}
+
+	public GameObject RaycastGUI (Vector2 pos) {
+		PointerEventData pointerData = new PointerEventData(EventSystem.current);
+		pointerData.position = pos;
+		List<RaycastResult> results = new List<RaycastResult>();
+		m_GraphicRaycaster.Raycast(pointerData, results);
+
+		if (results.Count > 0) {
+			foreach(RaycastResult result in results)
+			{
+				if(result.gameObject.GetComponentInParent<ARInteractable>())
+				{
+					Debug.Log("Found interactable");
+					return result.gameObject;
+				}
+			}
+		}
+		return null;
 	}
 
 	public bool Raycast (Vector2 pos) {
