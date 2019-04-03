7c4b320fe8a4c82a98bde3091365cf4ee9bf6dcf
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2019-04-03 17:26:20 -0600

    VideoInstallation aspect ratio, image recognition

---
+++ ARCore_Test/Assets/Prefabs/Video/VideoInstallation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoInstallation : ARInteractable {
+
+	public new AugmentedImage Image;
+
+	public VideoPlayer vp;
+	public GameObject plane;
+	public float width;
+
+	private void Awake() {
+		float aspectRatio = (float) vp.clip.width / (float) vp.clip.height;
+
+        Vector3 scale = plane.transform.localScale;
+		scale.z = scale.x / aspectRatio;
+
+        plane.transform.localScale = scale;
+	}
+}
