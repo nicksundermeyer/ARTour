e75436d1eb8ca326da5f23e7fbb3d86a94cdd185
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2019-04-03 19:18:14 -0600

    Video mostly working aside from slider

--- ARCore_Test/Assets/Prefabs/Video/VideoInstallation.cs
+++ ARCore_Test/Assets/Prefabs/Video/VideoInstallation.cs
@@ -14,12 +14,60 @@ public class VideoInstallation : ARInteractable {
 	public GameObject plane;
 	public float width;
 
-	private void Awake() {
+	[Header ("Controls")]
+	public float skipTime;
+	public Button playButton;
+	public Button pauseButton;
+	public Button nextButton;
+	public Button previousButton;
+	public Slider progressBar;
+
+	// button click boolean events
+	private bool playClicked = false;
+	private bool pauseClicked = false;
+	private bool nextClicked = false;
+	private bool previousClicked = false;
+
+	private void Awake () {
 		float aspectRatio = (float) vp.clip.width / (float) vp.clip.height;
 
-        Vector3 scale = plane.transform.localScale;
+		Vector3 scale = plane.transform.localScale;
 		scale.z = scale.x / aspectRatio;
 
-        plane.transform.localScale = scale;
+		plane.transform.localScale = scale;
+	}
+
+	private void Update () {
+		progressBar.value = (float) vp.frame / (float) vp.frameCount;
+	}
+
+	public void PlayButton () {
+		if (!playClicked) {
+			vp.Play ();
+			playButton.gameObject.SetActive (false);
+			pauseButton.gameObject.SetActive (true);
+		}
+		playClicked = !playClicked;
+	}
+
+	public void PauseButton () {
+		if (!pauseClicked) {
+			vp.Pause ();
+			playButton.gameObject.SetActive (true);
+			pauseButton.gameObject.SetActive (false);
+		}
+		pauseClicked = !pauseClicked;
+	}
+
+	public void NextButton () {
+		if (!nextClicked)
+			vp.time += skipTime;
+		nextClicked = !nextClicked;
+	}
+
+	public void PreviousButton () {
+		if (!previousClicked)
+			vp.time -= skipTime;
+		previousClicked = !previousClicked;
 	}
 }
